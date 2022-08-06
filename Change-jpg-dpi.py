01a9caa891f0c793342e5d3fe716ad35dca52ef6
chiahsun09 <runranrun0911@gmail.com>
2022-08-06 09:35:34 +0800

    20220806
    change save to save new file , not cover it

--- Change-jpg-dpi.py
+++ Change-jpg-dpi.py
@@ -1,27 +1,36 @@
 from PIL import Image
 import os
+'''
+目的:
+改資料夾內jpg圖片的dpi,並另存新檔後加dpi規格說明
 
-#改資料夾內jpg圖片的dpi
-#要換路徑的話,改dirName
+要設定參數說明:
+dirName:照片存檔的資料夾路徑
+newDip:要另存設定的的新dpi
+'''
+
 dirName = "C:\\Users\\runra\Desktop\\test\\"   #最後要加雙斜線,不然會報錯
 li=os.listdir(dirName)
 for ims in li:
     im = Image.open(dirName+ims)
     nx, ny = im.size
     f = im.format
-    print("原始x_dpi: ",nx,"原本x_dpi: ",ny,"圖片格式: ",f)
+    print("原始照片尺寸: ",nx,"x",ny,"圖片格式: ",f)
+    fileName=ims.split(".")
 
     if im.info.get('dpi'):
         x_dpi, y_dpi = im.info['dpi']
         if x_dpi != y_dpi:
             print('Inconsistent DPI image data')
-        print("x_dpi: " + str(x_dpi),"y_dpi: " + str(y_dpi))
+        print("原本x_dpi: " + str(x_dpi),"原本y_dpi: " + str(y_dpi))
     else:
         print('No DPI data. Invalid Image header')
         print(im.info)
-    #im2 = im.resize((int(nx*2.5), int(ny*2.5)), Image.BICUBIC)
-    #print(filename)
-    im.save(f"{dirName}/"+ims,dpi=(300,300),quality=100)
+    
+
+    newDpi=300  #設定新的dpi
+    newFileName=fileName[0]+"_"+str(newDpi)+"dpi"+".jpg"
+    im.save(f"{dirName}/"+newFileName,dpi=(newDpi,newDpi),quality=100)
 
 
-print("轉換完成")
+print("資料夾內檔案轉換完成")
